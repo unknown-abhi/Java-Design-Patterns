4df1aaf69771be512dd6166971889a6914931f35
Abhishek Kumar <ak35540@gmail.com>
2024-09-17 03:01:21 +0530

    CallableAndFutureDemo

---
+++ Callable_And_Future/CallableAndFutureDemo.java
@@ -0,0 +1,47 @@
+package Callable_And_Future;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class CallableAndFutureDemo {
+    public static void main(String[] args) {
+        // Create a Callable task
+        ExecutorService executor = Executors.newCachedThreadPool();
+
+        Callable<String> callable = () -> {
+
+            Thread.sleep(5000);
+            return "Result return from :: " + Thread.currentThread().getName();
+        };
+
+        // Submit the Callable task to the ExecutorService
+        Future<String> future = executor.submit(callable);
+
+        // Retrieve the result from the Future object
+        try {
+            System.out.println("Result from Callable: " + future.get());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // Shut down the ExecutorService
+        executor.shutdown();
+
+        // Check if ExecutorService is terminated
+        while (!executor.isTerminated()) {
+            // Wait for a while before checking again
+            try {
+                System.out.println("ExecutorService waiting to terminated");
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        System.out.println("ExecutorService is terminated");
+
+    }
+
+}
